330b5c8a1ec86f9d55d8b0dee6822d41ccdc7f40
youngchan <ecker@naver.com>
2020-09-30 11:52:14 +0900

    at milk

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world! - First.")
+        Text("Hello, world! - milk version.")
             .padding()
     }
 }
